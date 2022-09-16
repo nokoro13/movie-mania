de74e120048812ce99d5c6a12b558376cdc5ea88
nokoro13 <109981603+nokoro13@users.noreply.github.com>
2022-09-16 13:56:32 -0700

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,21 +7,22 @@ const MOVIE_POSTER = 'https://image.tmdb.org/t/p/w500';
 const Movie = (props) => {
     
     const FavouriteComponent = props.favouriteComponent;
+    const movieInfo = props.movie;
     
     return (
     <div className='movie'>
-        <img src={MOVIE_POSTER + props.poster_path} alt={props.title} />
+        <img src={MOVIE_POSTER + movieInfo.poster_path} alt={movieInfo.title} />
 
         <div className='movie-data'>
-            <h3 className='movie-title'>{props.title}</h3>
+            <h3 className='movie-title'>{movieInfo.title}</h3>
                 
-                <p>Rating: {props.vote_average}</p>
-                <p>Release Date: {props.release_date}</p>
+                <p>Rating: {movieInfo.vote_average}</p>
+                <p>Release Date: {movieInfo.release_date}</p>
                 
                 <div className='synopsis' >
                     <i className='heart-icon' onClick={()=>props.handleFavsClick(props)}><FavouriteComponent  favouriteComponent={AddFavourites}/></i>
                     <h2>Synopsis:</h2>
-                    <p> {props.overview}</p>
+                    <p> {movieInfo.overview}</p>
                 </div>
 
         </div>

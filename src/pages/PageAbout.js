2ff5e9b839dc52a02bb47df18345acfafabc936f
emerson <connors.emerson@gmail.com>
2022-09-17 14:26:28 -0700

    about

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,18 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import "../about.css";
+import tmdbLogo from "../images/tmdb-logo.svg";
 
 const PageAbout = () => {
+  useEffect(() => {
+    document.title = `About`;
+  }, []);
 
-    useEffect(() => {
-		document.title = `About`;
-	}, []);
-
-    return (
-        <section className='page-heading'>
-            <h2>About page</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nihil, doloremque illum aperiam dolor, est aliquid officia reprehenderit atque voluptatibus rerum pariatur earum nisi quasi modi, enim harum totam rem!</p>
-        </section>
-    )
-
-}
+  return (
+    <section className="page-heading">
+      <section className="about-body">
+        <h2>About page</h2>
+        <p>
+          Moovy is a movie database website that displays movies based upon the
+          users preferences. We feature movies from around the world, old and
+          new a like. We strive to make finding movies a simple and easy
+          process.
+        </p>
+        <p>
+          This product uses the TMDb API but is not endorsed or certified by
+          TMDb.
+        </p>
+        <div className="tmdb-flex">
+          <img src={tmdbLogo} alt="tmdb logo" className="tmdb-logo-img" />
+        </div>
+      </section>
+    </section>
+  );
+};
 
 export default PageAbout;
